8319d4cf3c83dce49c5644c0cbb79dece9805ae5
Gabby D <gabby529md@gmail.com>
2025-10-10 08:33:38 +0100

    Accelerometer
    Accelerometer

---
+++ Accelerometer071025/Accelerometer071025.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <MPU6050.h>
+MPU6050 mpu;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.initialize();
+}
+void loop() {
+  int16_t ax, ay, az;
+  mpu.getAcceleration(&ax, &ay, &az);
+  Serial.print("X: ");
+  Serial.print(ax);
+  Serial.print(" | Y: ");
+  Serial.print(ay);
+  Serial.print(" | Z: ");
+  Serial.println(az);
+  delay(500);
+}
